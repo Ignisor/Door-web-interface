c4fcc47322c450f0f337baca1e61d5e553ef26af
ignisor <ignis2497@gmail.com>
2018-10-04 17:06:50 +0300

    production settings

--- door_web/settings.py
+++ door_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xrf$*q#0g5x5(wx%58z2_2gruzslvcs+sm1bsmh%mpq#1%@f*f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # IoT settings
 DOOR_MODULE_URL = 'http://192.168.1.133/'
